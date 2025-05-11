ec67e230612cbe73d3be2deeb4cca8bfc882e6fe
Mohamed amine Tlili <foxweb585@gmail.com>
2025-05-11 23:32:00 +0200

    enhance recomm_job

--- spark/recommendation_job.py
+++ spark/recommendation_job.py
@@ -2,11 +2,12 @@ from pyspark.sql import SparkSession
 from pyspark.ml.recommendation import ALS
 from pyspark.sql.functions import col, explode
 from pyspark.sql.types import FloatType, IntegerType
+from pyspark.sql.functions import col, explode, avg
+from datetime import datetime
 
 # Initialize Spark session
 spark = SparkSession.builder \
     .appName("DariRecommendationSystem") \
-    .config("spark.jars", "mysql-connector-j-9.3.0.jar") \
     .getOrCreate()
 
 # DB connection config
@@ -30,6 +31,8 @@ als_data = interaction_df.select(
     col("user_id").cast(IntegerType()),
     col("annonce_id").cast(IntegerType()),
     col("interaction_score").cast(FloatType())
+).groupBy("user_id", "annonce_id").agg(
+    avg("interaction_score").alias("interaction_score")
 )
 
 print("Training recommendation model...")
@@ -40,20 +43,21 @@ als = ALS(
     itemCol="annonce_id",
     ratingCol="interaction_score",
     coldStartStrategy="drop",
-    rank=10,
+    rank=15,
     maxIter=15,
     regParam=0.1
 )
 als_model = als.fit(als_data)
 
 # --- Save the trained ALS model to disk ---
-als_model_path = "/app/models/als_model"  # Local path to save the ALS model
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+als_model_path = f"/app/models/als_model_{timestamp}"
 als_model.save(als_model_path)
 print(f"ALS model saved to {als_model_path}")
 
 # Generate recommendations for all users
 print("Generating recommendations...")
-user_recommendations = als_model.recommendForAllUsers(10)
+user_recommendations = als_model.recommendForAllUsers(100)
 
 # Transform the recommendations to a MySQL-compatible format
 print("Transforming recommendations for database storage...")
